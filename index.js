89d46ef32bb40ad33457ffd68585daca23df1696
Bikram Bhusal <bikrambhusal75@gmail.com>
2023-06-29 23:41:05 +0545

    learned about HTTP protocols

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("hello world!");
+const users = [];
+
+app.post('/signup', (req, res) => {
+    res.send("Signup page!");
+
+});
+
+app.post('/login', (req, res) => {
+    res.send("Login page!");
+});
+
+app.get('/questions', (req, res) => {
+    res.send("Questions page!");
+});
+
+app.get('/submissions', (req, res)=>{
+    res.send("Submissions page");
 });
 
 app.listen(port,()=>{
     console.log(`Server running at port ${port}`);
 });
+
