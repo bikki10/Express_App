becf3c6f4652c6c6b0d72f290f1a17b39f37b453
Bikki <bikrambhusal75@gmail.com>
2023-07-21 00:04:39 +0545

    Learned about OS node package

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
-
+const os = require("os");
 const port = 3000;
 
-const users = [];
+// const users = [];
 
-app.post('/signup', (req, res) => {
-    res.send("Signup page!");
+// app.post('/signup', (req, res) => {
+//     res.send("Signup page!");
 
+// });
+
+// app.post('/login', (req, res) => {
+//     res.send("Login page!");
+// });
+
+// app.get('/questions', (req, res) => {
+//     res.send("Questions page!");
+// });
+
+// app.get('/submissions', (req, res)=>{
+//     res.send("Submissions page");
+// });
+
+// const bikkiApi = require("./routes/route.api"); // Yaha chai module import gareko, routing gareko vanxa yeslai
+
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.platform());
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`);
 });
-
-app.post('/login', (req, res) => {
-    res.send("Login page!");
-});
-
-app.get('/questions', (req, res) => {
-    res.send("Questions page!");
-});
-
-app.get('/submissions', (req, res)=>{
-    res.send("Submissions page");
-});
-
-app.listen(port,()=>{
-    console.log(`Server running at port ${port}`);
-});
-
---
+++ routes/route.api.js
@@ -0,0 +1,11 @@
+const bikki = {
+    name: "Bikram",
+    age: 22,
+    location: "Kathmandu",
+    single: true
+};
+
+console.log(bikki);
+
+module.exports =bikki;  // Yoh chai module export gareko, yeslai routing pani vanxa    
+                        // Feri pani vandai xu, module.exports maa = laagxa . haina
